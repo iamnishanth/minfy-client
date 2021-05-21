35a36bf5a1b53c5cbd757ed2847b852c0a3634d5
iamnishanth <nishanthvenk68@gmail.com>
2021-05-22 00:14:32 +0530

    Added loading state in Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,50 +7,54 @@ const Dashboard = () => {
     thirtyDaysRecord: [],
     oneDayRecord: [],
   });
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       let res = await fetchLinks();
       setLinks(res);
       let date = await getCount();
       setCount(date);
+      setLoading(false);
     };
     fetchData();
   }, []);
   return (
     <div className="text-center">
-      <div className="form-signin">
-        <div>
-          <h4 className="mb-2">
-            URL shortened today - {count.oneDayRecord.length}
-          </h4>
-          <h4 className="mb-2">
-            URL shortened this month - {count.thirtyDaysRecord.length}
-          </h4>
+      {!loading && (
+        <div className="form-signin">
+          <div>
+            <h4 className="mb-2">
+              URL shortened today - {count.oneDayRecord.length}
+            </h4>
+            <h4 className="mb-2">
+              URL shortened this month - {count.thirtyDaysRecord.length}
+            </h4>
+          </div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Short URL</th>
+                <th>Long URL</th>
+                <th>Total Hits</th>
+                <th>Created At</th>
+              </tr>
+            </thead>
+            <tbody>
+              {links.map((link) => {
+                return (
+                  <tr key={link._id}>
+                    <td>{link.shortURL}</td>
+                    <td>{link.longURL}</td>
+                    <td>{link.hitCount}</td>
+                    <td>{new Date(link.createdAt).toLocaleDateString()}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Short URL</th>
-              <th>Long URL</th>
-              <th>Total Hits</th>
-              <th>Created At</th>
-            </tr>
-          </thead>
-          <tbody>
-            {links.map((link) => {
-              return (
-                <tr key={link._id}>
-                  <td>{link.shortURL}</td>
-                  <td>{link.longURL}</td>
-                  <td>{link.hitCount}</td>
-                  <td>{new Date(link.createdAt).toLocaleDateString()}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      )}
+      {loading && <h3>Loading....</h3>}
     </div>
   );
 };
